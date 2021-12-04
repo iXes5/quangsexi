fe8fd8ea6787571451059a77286b2fe6c2d3007f
iXes <quangsexy01@gmail.com>
2021-12-04 23:32:25 +0700

    big update

--- basic.js
+++ basic.js
@@ -1,33 +1,54 @@
 function send() {
     var arr = document.getElementsByTagName('input');
     var name =  arr[0].value;
-    var check1 = arr[3].checked;
-    var check2 = arr[4].checked;
-    var check3 = arr[5].checked;
-    var check4 = arr[6].checked;
+    var age = arr[1].value;
+    var email = arr[2].value;
+    var check1 = arr[5].checked;
+    var check2 = arr[6].checked;
+    var check3 = arr[7].checked;
+    var check4 = arr[8].checked;
     var gender = "";
     var favorite = "";
+    var arrString = email.split("");
+
     if (arr[1].checked) {
-        gender = arr[1].value;
+        gender = arr[3].value;
     }else {
-        gender = arr[2].value;
+        gender = arr[4].value;
     }
-    for(var i=3; i<=6; i++) {
+
+    for(var i=5; i<=8; i++) {
         if (arr[i].checked) {
             favorite = favorite + arr[i].value + " ";
         }
     }
-    if (name == "") {
-        alert('Hãy điền tên của bạn!');
+
+    if (name == "" || email == "" || age == "") {
+        alert('Hãy điền đầy đủ thông tin!');
         return;
     }
+
+    if (isNaN(age)) {
+        alert("Tuổi phải là một con số!");
+        return;
+    }else if (age<=0) {
+        alert("Tuổi phải là số dương!");
+        return;
+    }
+
+    if (arrString.includes("@", ".") == false) {
+        alert("Đây không phải là một địa chỉ email!");
+        return;    
+    }
+
     if (check1||check2||check3||check4) {
 
     }else {
         alert('Bạn phải mua hàng chứ:v');
         return;
     }
-    var choice =  confirm("Đã xác nhận thông tin của bạn\n"+"Họ tên: "+name+"\n"+"Giới tính: "+gender+"\n"+"Đã mua: "+favorite);
+
+    var choice =  confirm("Đã xác nhận thông tin của bạn\n"+"Họ tên: "+name+"\n"+"Tuổi: "+age+"\n"+"Địa chỉ email: "+email+"\n"+"Giới tính: "+gender+"\n"+"Đã mua: "+favorite);
     if(choice == 1){
         alert('Thông tin đã được gửi');
     }
