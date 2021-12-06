6aae235fb4375bb55fa25f3f05b7ba6c82da99a7
iXes <quangsexy01@gmail.com>
2021-12-06 20:54:43 +0700

    project complerted

--- basic.js
+++ basic.js
@@ -1,33 +1,40 @@
 function send() {
-    var arr = document.getElementsByTagName('input');
+    var arr = document.getElementsByTagName("input");
     var name =  arr[0].value;
     var age = arr[1].value;
     var email = arr[2].value;
-    var check1 = arr[5].checked;
-    var check2 = arr[10].checked;
-    var check3 = arr[15].checked;
-    var check4 = arr[20].checked;
+    var check = 0;
+    var check1 = arr[21].checked;
+    var check2 = arr[22].checked;
+    var check3 = arr[23].checked;
     var gender = "";
-    var favorite = "";
+    var test = "";
     var arrString = email.split("");
-
-    if (arr[1].checked) {
+    var arrCost = [30000, 36000, 35000, 40000, 24000, 25000, 28000, 27000, 65000, 62000, 60000, 64000, 5000, 7000, 5000, 10000];
+    var cost = 0;
+    var product = "";
+    var j = 0;
+    //Giới tính
+    if (arr[3].checked) {
         gender = arr[3].value;
     }else {
         gender = arr[4].value;
     }
 
-    for(var i=5; i<=20; i=i+5) {
+    //Mặt hàng đã mua
+    for (var i=21; i<=23; i=i+1) {
         if (arr[i].checked) {
-            favorite = favorite + arr[i].value + " ";
+            test = test + arr[i].value + " ";
         }
     }
 
+    //Điển thông tin
     if (name == "" || email == "" || age == "") {
         alert('Hãy điền đầy đủ thông tin!');
         return;
     }
 
+    //Kiểm tra tuổi 
     if (isNaN(age)) {
         alert("Tuổi phải là một con số!");
         return;
@@ -36,24 +43,65 @@ function send() {
         return;
     }
 
-    if (arrString.includes("@", ".") == false) {
+    //Kiểm tra email
+    if (arrString.includes("@") == false || arrString.includes(".") == false) {
         alert("Đây không phải là một địa chỉ email!");
         return;    
     }
 
-    if (check1||check2||check3||check4) {
-
-    }else {
-        alert('Bạn phải mua hàng chứ:v');
+    //Kiểm tra mua hàng
+    for (var i=5; i<=20; i=i+1) {
+        if (arr[i].value == "") {
+            
+        }else {
+            check = check + 1;
+            product = product + arr[i].name + ": " + arr[i].value + "\n";
+        };
+        if (arr[i].value == "") {
+            
+        }else if (isNaN(arr[i].value)) {
+            alert("Đơn hàng "+arr[i].name+" phải là số!");
+            return;
+        }else if (arr[i].value <= 0) {
+            alert("Đơn hàng phải là một số dương!");
+            return;
+        };
+    }
+    if (check == 0) {
+        alert("Bạn phải mua hàng chứ:v");
         return;
     }
 
-    var choice =  confirm("Đã xác nhận thông tin của bạn\n"+"Họ tên: "+name+"\n"+"Tuổi: "+age+"\n"+"Địa chỉ email: "+email+"\n"+"Giới tính: "+gender+"\n"+"Đã mua: "+favorite);
-    if(choice == 1){
+    //Kiểm tra dùng thử
+    if (check1||check2||check3) {
+
+    }else {
+        alert("Hãy chọn một sản phẩm dùng thử!");
+        return;
+    }
+
+    //Tính bill
+    for (var i=5; i<=20; i=i+1) {
+        if (arr[i].value != "") {
+            cost = cost + (arr[i].value * arrCost[j])
+        };
+        j = j + 1;
+    }
+
+    //Lấy thời gian
+    var today = new Date();
+    var clock = today.getHours()+"/"+today.getMinutes()+"/"+today.getSeconds()+"-"+today.getDate()+"/"+today.getMonth()+"/"+today.getFullYear();
+
+    //Báo cáo thông tin
+    var choice1 = confirm("Đã xác nhận thông tin của bạn\n"+"Họ tên: "+name+"\n"+"Tuổi: "+age+"\n"+"Giới tính: "+gender+"\n"+"Địa chỉ email: "+email);
+    var choice2 = confirm("Đơn mua hàng: \n"+product+"Tổng cộng: "+cost+"vnđ"+"\n"+"Sản phẩm dùng thử: "+test+"\n"+"Hãy dùng thử và đóng góp ý kiến cho chúng tôi nhé<3\n"+"("+clock+")");
+    if (choice1 == 1) {
         alert('Thông tin đã được gửi');
     }
+    document.getElementById("time").innerHTML = "Đơn giá: " + cost + " (" + clock + ")"
 }
 
+//Reset dữ liệu
 function resetForm(){
     document.getElementsByTagName('form')[0].reset();
 }
